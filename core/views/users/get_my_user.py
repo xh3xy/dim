8c7bf2895946c349216e8d8af80781a3b61424af
xh3xy <116487192+xh3xy@users.noreply.github.com>
2026-05-09 17:06:23 +0200

    fix: replace hardcoded values with db lookups

--- core/views/users/get_my_user.py
+++ core/views/users/get_my_user.py
@@ -1,33 +1,33 @@
 from core.decorators.is_authenticated import is_authenticated
-from django.utils.decorators import method_decorator
-from django.http import JsonResponse
-from django.views import View
-from typing import cast
 from core.models.user import User
 from core.utils.timezone_serializer import timezone_serializer
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from typing import cast
 
 
 @method_decorator(is_authenticated, name='dispatch')
 class GetMyUser(View):
     def get(self, request):
-        user = request.user
+        user = cast(User, request.user)
 
         return JsonResponse({
             'id': user.id,
             'email': user.email,
             'name': user.name,
             'profileImagePath': user.profile_image if user.profile_image else '',
-            'avatarColor': 'blue', #TODO: Hardcoded
+            'avatarColor': user.avatar_color,
             'profileChangedAt': timezone_serializer(user.profile_changed_at),
-            'storageLabel': 'admin', #TODO: Hardcoded
+            'storageLabel': user.storage_label,
             'shouldChangePassword': user.should_change_password,
             'isAdmin': user.is_admin,
             'createdAt': timezone_serializer(user.created_at),
-            'deletedAt': None, #TODO: Hardcoded
+            'deletedAt': timezone_serializer(user.deleted_at) if user.deleted_at else user.deleted_at,
             'updatedAt': timezone_serializer(user.updated_at),
-            'oauthId': '', #TODO: Hardcoded
-            'quotaSizeInBytes': None, #TODO: Hardcoded
-            'quotaUsageInBytes': 193684647845, #TODO: Hardcoded
+            'oauthId': user.oauth_id,
+            'quotaSizeInBytes': user.quota_size_in_bytes,
+            'quotaUsageInBytes': user.quota_usage_in_bytes,
             'status': user.status,
             'license': None, #TODO: Hardcoded
         })
