06582038c1737bb9edc56300c5a42d57ec0a1951
xh3xy <116487192+xh3xy@users.noreply.github.com>
2026-05-12 23:09:58 +0200

    fix: correctly return uuid

--- core/views/api_keys/api_keys.py
+++ core/views/api_keys/api_keys.py
@@ -1,11 +1,11 @@
 from core.decorators.is_authenticated import is_authenticated
-from django.utils.decorators import method_decorator
-from django.views import View
-from django.http import JsonResponse
 from core.models.api_key import ApiKey
-from typing import cast
 from core.models.session import Session
 from core.utils.timezone_serializer import timezone_serializer
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from typing import cast
 
 
 @method_decorator(is_authenticated, name='dispatch')
@@ -17,13 +17,13 @@ class ApiKeys(View):
 
         return JsonResponse([
             {
-                "id": key.id,
-                "name": key.name,
-                "createdAt": timezone_serializer(key.created_at),
-                "updatedAt": timezone_serializer(key.updated_at),
+                'id': str(key.id),
+                'name': key.name,
+                'createdAt': timezone_serializer(key.created_at),
+                'updatedAt': timezone_serializer(key.updated_at),
                 # TODO: Hardcoded
-                "permissions": [
-                    "all"
-                ]
+                'permissions': [
+                    'all'
+                ],
             } for key in keys
         ], safe=False)
